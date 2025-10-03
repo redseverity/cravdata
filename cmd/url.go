463056bf54485687293ff874b74812c3940aa3e7
redseverity <red.severity723@passfwd.com>
2025-10-02 21:31:30 -0300

    feat(cmd/url): add validation to ensure at least one argument is provided
    Signed-off-by: redseverity <red.severity723@passfwd.com>

--- cmd/url.go
+++ cmd/url.go
@@ -4,15 +4,40 @@ Copyright © 2025 redseverity <red.severity723@passfwd.com>
 package cmd
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/spf13/cobra"
+)
+
+var (
+	URL = ""
 )
 
 // urlCmd represents the url command
 var urlCmd = &cobra.Command{
-	Use: "url",
+	Use: "url [URL]",
 	// Short: "",
 	// Long:  ``,
-	// Run: func(cmd *cobra.Command, args []string) {},
+	Args: func(cmd *cobra.Command, args []string) error {
+		var rawURL = args[0]
+
+		// Validate that the command receives at least one argument.
+		if err := cobra.MinimumNArgs(1)(cmd, args); err != nil {
+			return fmt.Errorf("%s\n ", err.Error())
+		}
+
+		//parsing url
+		if !strings.HasPrefix(rawURL, "http://") && !strings.HasPrefix(rawURL, "https://") {
+			URL = "https://" + rawURL
+		}
+
+		return nil
+	},
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
 }
 
 func init() {
