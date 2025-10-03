6916422c4034a16bd8beb300f8c4917df322ca5f
redseverity <red.severity723@passfwd.com>
2025-10-03 03:20:38 -0300

    feat(validators/urlcheck): create RequireMinArgs to replace direct Args validation in url.go
    Signed-off-by: redseverity <red.severity723@passfwd.com>

--- cmd/url.go
+++ cmd/url.go
@@ -4,34 +4,19 @@ Copyright © 2025 redseverity <red.severity723@passfwd.com>
 package cmd
 
 import (
-	"fmt"
-	"strings"
-
+	"github.com/redseverity/cravdata/cmd/validators/urlcheck"
 	"github.com/spf13/cobra"
-)
-
-var (
-	URL = ""
 )
 
 // urlCmd represents the url command
 var urlCmd = &cobra.Command{
-	Use: "url [URL]",
+	Use: "url [target-url]",
 	// Short: "",
 	// Long:  ``,
 	Args: func(cmd *cobra.Command, args []string) error {
-		var rawURL = args[0]
-
-		// Validate that the command receives at least one argument.
-		if err := cobra.MinimumNArgs(1)(cmd, args); err != nil {
-			return fmt.Errorf("%s\n ", err.Error())
+		if err := urlcheck.RequireMinArgs(cmd, args, 1); err != nil {
+			return err
 		}
-
-		//parsing url
-		if !strings.HasPrefix(rawURL, "http://") && !strings.HasPrefix(rawURL, "https://") {
-			URL = "https://" + rawURL
-		}
-
 		return nil
 	},
 
---
+++ cmd/validators/urlcheck/requireMinArgs.go
@@ -0,0 +1,18 @@
+/*
+Copyright © 2025 redseverity <red.severity723@passfwd.com>
+*/
+package urlcheck
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// RequireMinArgs ensures args has at least n elements.
+func RequireMinArgs(cmd *cobra.Command, args []string, n int) error {
+	if err := cobra.MinimumNArgs(n)(cmd, args); err != nil {
+		return fmt.Errorf("%s\n ", err)
+	}
+	return nil
+}
